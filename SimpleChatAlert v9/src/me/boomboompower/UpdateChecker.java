00e0e0cfd14f2ca30c323325478b2945c95ccb55
BuildTools <boomboompowerjunk@gmail.com>
2016-03-09 20:33:25 +1100

    Fixed UpdateChecker

--- SimpleChatAlert v9/src/me/boomboompower/UpdateChecker.java
+++ SimpleChatAlert v9/src/me/boomboompower/UpdateChecker.java
@@ -1,67 +1,59 @@
+
 package me.boomboompower;
 
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class UpdateChecker
-{
-  private URL filesFeed;
-  private String version;
-  private String lastversion;
-  private String link;
-  
-  public UpdateChecker(String version)
-  {
-    this.version = version;
-    try
-    {
-      this.filesFeed = new URL("http://dev.bukkit.org/bukkit-plugins/Simple-Chat-Alert/files.rss");
-    }
-    catch (MalformedURLException e)
-    {
-      e.printStackTrace();
-    }
-  }
-  
-  public boolean updateNeeded()
-  {
-    try
-    {
-      InputStream input = this.filesFeed.openConnection().getInputStream();
-      Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
-      
-      Node latestFile = document.getElementsByTagName("item").item(0);
-      NodeList children = latestFile.getChildNodes();
-      
-      this.lastversion = children.item(1).getTextContent().replaceAll("[^0-9.]", "");
-      this.link = children.item(3).getTextContent();
-      
-      return !this.version.equals(this.lastversion);
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-    return false;
-  }
-  
-  public String getCurrentVersion()
-  {
-    return this.version;
-  }
-  
-  public String getLastestVersion()
-  {
-    return this.lastversion;
-  }
-  
-  public String getLink()
-  {
-    return this.link;
-  }
+public class UpdateChecker {
+
+	private URL filesFeed;
+
+	private String version, lastversion, link;
+
+	public UpdateChecker(String version) {
+		this.version = version;
+		
+		try {
+			filesFeed = new URL("http://dev.bukkit.org/bukkit-plugins/Simple-Chat-Alert/files.rss");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean updateNeeded() {
+		try {
+			InputStream input = filesFeed.openConnection().getInputStream();
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
+
+			Node latestFile = document.getElementsByTagName("item").item(0);
+			NodeList children = latestFile.getChildNodes();
+
+			lastversion = children.item(1).getTextContent().replaceAll("[^0-9.]", "");
+			link = children.item(3).getTextContent();
+
+			return !version.equals(lastversion);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public String getCurrentVersion() {
+		return version;
+	}
+	
+	public String getLastestVersion() {
+		return lastversion;
+	}
+	
+	public String getLink() {
+		return link;
+	}
 }
